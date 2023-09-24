421a5ba9905804f3c155103d3a96a713f534fb3f
JSterling56 <jtsterlin@yahoo.com>
2023-09-24 15:16:15 -0400

    added more formatting files/.env/gitignore, added apollog server & other relevant backend, starting frontend

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const typeDefs = require('./schemas');
+const resolvers = require ('./resolvers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
