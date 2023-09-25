a50255bd60d5236dc845a0a8e0f467c69231f263
JSterling56 <jtsterlin@yahoo.com>
2023-09-25 11:22:42 -0400

    started front end

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SavedBookInput!) {
+        saveBook(book: $book) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                forSale
+                link
+                title
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                _id
+                authors
+                description
+                bookID
+                image
+                forSale
+                link
+                title
+            }
+        }
+    }
+    `;
