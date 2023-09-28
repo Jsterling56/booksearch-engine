d0fe4e9f9c951cf8ca41613fb9323f86be1b417c
JSterling56 <jtsterlin@yahoo.com>
2023-09-28 18:06:48 -0400

    working on front end pages

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,37 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import client from './apollo';
 import Navbar from './components/Navbar';
+import { saveBook, searchGoogleBooks } from './utils/API';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql"
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={searchGoogleBooks} />
+            <Route exact path='/saved' component={saveBook} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+      </Switch>
+      </>
+    </Router>
+    </ApolloProvider >
   );
 }
 
