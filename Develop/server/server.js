b0614520350ae99ed188b7872711d2430ae50572
JSterling56 <jtsterlin@yahoo.com>
2023-09-29 14:07:15 -0400

    troubleshooint askBCS

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -79,3 +79,42 @@ startApolloServer();
 //   app.listen(PORT, () => 
 //   console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 // });
+
+
+// const express = require('express');
+// const path = require('path');
+// const { ApolloServer } = require('apollo-server-express'); // Use 'apollo-server-express' here
+// const { authMiddleware } = require('./utils/auth');
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Create a new instance of an Apollo server with the GraphQL schema
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware, // Include context here
+// });
+
+// // Apply Apollo Server middleware to the '/graphql' path
+// server.applyMiddleware({ app, path: '/graphql' });
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//   });
+// }
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`GraphQL at http://localhost:${PORT}/graphql`);
+//   });
+// });
